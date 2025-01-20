b43d304ed3298d7c1cbac074e6d170c4a1a54a36
porteenbois <porteenbois@users.noreply.github.com>
2025-01-20 09:22:16 +0100

    debut formulaire creer offre

---
+++ src/Controller/FormulaireCreerOffreController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use App\Form\FormulaireCreerOffreType;
+use App\Entity\Offres; 
+
+final class FormulaireCreerOffreController extends AbstractController
+{
+    #[Route('/formulaire/creer/offre', name: 'app_formulaire_creer_offre')]
+    public function index(Request $request): Response
+    {
+        $offre = new Offres();
+        
+        $form = $this->createForm(FormulaireCreerOffreType::class, $offre);
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($offre);
+            $entityManager->flush();
+        
+            return $this->redirectToRoute('some_route_name');
+        }
+
+        return $this->render('formulaire_creer_offre/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
