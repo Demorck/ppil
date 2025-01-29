2d74c97c7975ecd63b828d10bd4e448f055c58e2
Demorck <57636452+Demorck@users.noreply.github.com>
2025-01-29 19:53:59 +0100

    Ajout du header en fonction de locataire/propriétaire (ou les deux) et du footer

--- assets/app.ts
+++ assets/app.ts
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-
+    let icon_profile = document.getElementById("user-menu-button") as HTMLButtonElement;
+    if (icon_profile != null) {
+        icon_profile.addEventListener('click', dropdown_menu_profile);
+    }
 })
+
+function dropdown_menu_profile() {
+    let dropdown = document.getElementById('dropdown-menu-profile')!;
+    if (dropdown.classList.contains('active')) {
+        dropdown.classList.add('hidden');
+        dropdown.classList.remove('active');
+    } else {
+        dropdown.classList.remove('hidden');
+        dropdown.classList.add('active');
+    }
+}
