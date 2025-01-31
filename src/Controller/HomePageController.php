f8f0084a789b9dd1eb542e32b51ade0787b8a1f6
houssein <saban.houssein1@gmail.com>
2025-01-31 16:07:30 +0100

    ajout contrainte de date

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,15 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use DateTime;
 
 class HomePageController extends AbstractController
 {
     #[Route('', name: 'app_home_page')]
     public function index(): Response
     {
+        $dateAujourdhui = (new DateTime())->format('Y-m-d\TH:i');
+
         return $this->render('home_page/index.html.twig', [
             'title' => "Accueil",
             'controller_name' => 'HomePageController',
+            'dateAujourdhui' => $dateAujourdhui
         ]);
     }
 } 
