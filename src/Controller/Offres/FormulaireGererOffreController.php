d7cf291587dfaed8f082c30393ccaf104da39acb
porteenbois <porteenbois@users.noreply.github.com>
2025-02-24 10:49:03 +0100

    fin gerer offre

--- src/Controller/Offres/FormulaireGererOffreController.php
+++ src/Controller/Offres/FormulaireGererOffreController.php
@@ -4,42 +4,43 @@ namespace App\Controller\Offres;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
 use App\Form\FormulaireCreerOffreType;
 use App\Entity\Offres;
 
 final class FormulaireGererOffreController extends AbstractController
 {
-    #[Route('/offre/gerer', name: 'app_formulaire_gerer_offre')]
-    public function index(Request $request, EntityManagerInterface $entityManager): Response
+    #[Route('/offre/gerer/{id}', name: 'app_formulaire_gerer_offre')]
+    public function index(Request $request, EntityManagerInterface $entityManager, int $id): Response
     {
         if ($this->getUser() === null) {
             return $this->redirectToRoute('app_login');
         }
 
-        $offre = new Offres();
+        $offre = $entityManager->getRepository(Offres::class)->find($id);
+
+        if (!$offre) {
+            throw $this->createNotFoundException('Offre non trouvée.');
+        }
+
         $user = $this->getUser();
-        
+
         $form = $this->createForm(FormulaireCreerOffreType::class, $offre, [
             'user' => $user,
         ]);
         $form->handleRequest($request);
-        
+
         if ($form->isSubmitted() && $form->isValid()) {
-            $offre->setStatut(0);
-            $entityManager->persist($offre);
             $entityManager->flush();
-        
-            return $this->redirectToRoute('app_formulaire_vehicules');
+            return $this->redirectToRoute('app_liste_offres');
         }
 
         return $this->render('formulaire_gerer_offre/index.html.twig', [
-            'form' => $form->createView(),
-            'title' => "Gerer une offre",
+            'form'  => $form->createView(),
+            'title' => "Gérer une offre",
+            'offre' => $offre,
         ]);
     }
 }
