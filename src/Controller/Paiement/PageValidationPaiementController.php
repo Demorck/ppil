f35a1110142012f32c8d06edbaaad9e097b46b78
Elève <gregoire.fragnieres2@etu.univ-lorraine.fr>
2025-03-11 13:24:36 +0100

    Changement léger

--- src/Controller/Paiement/PageValidationPaiementController.php
+++ src/Controller/Paiement/PageValidationPaiementController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Paiement;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +11,12 @@ class PageValidationPaiementController extends AbstractController
     #[Route('/validationPaiement', name: 'app_page_validation_paiement')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('page_validation_paiement/index.html.twig', [
             'controller_name' => 'PageValidationPaiementController',
         ]);
