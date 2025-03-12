6276355820bac3f9ae669dbe7f763f6beb175a2d
porteenbois <porteenbois@users.noreply.github.com>
2025-03-12 15:46:55 +0100

    liste des litiges fini

--- src/Controller/Litiges/ListeLitigeController.php
+++ src/Controller/Litiges/ListeLitigeController.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace App\Controller\Paiements;
+namespace App\Controller\Litiges;
 
 use App\Entity\Locations;
-use App\Entity\Paiements;
+use App\Entity\Litiges;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ListePaiementController extends AbstractController
+class ListeLitigeController extends AbstractController
 {
-    #[Route('/paiements', name: 'app_liste_paiement')]
+    #[Route('/litiges', name: 'app_litiges')]
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         if ($this->getUser() === null) {
@@ -21,13 +21,13 @@ final class ListePaiementController extends AbstractController
         $user = $this->getUser();
 
         $locations = $entityManager->getRepository(Locations::class)->findBy(['locataire' => $user]);
-        $paiements = $entityManager->getRepository(Paiements::class)->findBy(['location' => $locations]);
+        $litiges = $entityManager->getRepository(Litiges::class)->findBy(['location' => $locations]);
 
-        dump($paiements);
+        dump($litiges);
 
-        return $this->render('liste_paiement/index.html.twig', [
-            'controller_name' => 'ListePaiementController',
-            'paiements' => $paiements,   
+        return $this->render('liste_litige/index.html.twig', [
+            'controller_name' => 'litigesController',
+            'litiges' => $litiges,   
         ]);
     }
 }
