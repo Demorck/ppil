8e59c6e6bd38c1068cc5b0bd585cf80a3cfc607f
Viloof <tristan1302@hotmail.fr>
2025-03-13 09:18:28 +0100

    Tests Accueil

--- src/Controller/Litiges/LitigesController.php
+++ src/Controller/Litiges/LitigesController.php
@@ -23,7 +23,7 @@ class LitigesController extends AbstractController
         $userid = $currentUser->getId();
         $location_id = $entMan->getRepository(Locations::class)->find($id1);
 
-        $litige = $entMan->getRepository(Litiges::class)->findOneBy(['id' => $location_id]);
+        $litige = $entMan->getRepository(Litiges::class)->findOneBy(['id' => $id, 'location' => $location_id]);
 
         if ($litige === null) {
             return $this->redirectToRoute('app_home_page');
